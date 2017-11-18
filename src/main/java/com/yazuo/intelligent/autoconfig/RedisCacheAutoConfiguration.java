3f4835802322d20a92aeab9b3f2e51288bb013d9
scvzerng <842494771@qq.com>
2017-11-19 01:11:15 +0800

    去除无用代码

--- src/main/java/com/yazuo/intelligent/autoconfig/RedisCacheAutoConfiguration.java
+++ src/main/java/com/yazuo/intelligent/autoconfig/RedisCacheAutoConfiguration.java
@@ -5,7 +5,6 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.yazuo.intelligent.cache.annotation.EnableTimeCaching;
 import com.yazuo.intelligent.cache.annotation.Time;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
